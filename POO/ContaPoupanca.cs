8174984805e60a1be437cd1048513233ccd92220
Brendo Santiago <brendoosantiago@gmail.com>
2017-06-25 22:42:18 -0300

    Criação de Projeto e Cenário de Classes

---
+++ POO/ContaPoupanca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class ContaPoupanca
+    {
+        private int agencia;
+        private int numeroConta;
+        private decimal saldo;
+
+        void Sacar(decimal valor)
+        {
+            saldo -= valor;
+        }
+
+        void Depositar (decimal valor)
+        {
+            saldo += valor;
+        }
+    }
+}
